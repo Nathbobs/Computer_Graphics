41a1df04a8d30e319601c8f2f8086562568bc0b8
nathbobs <your_noreply_email@users.noreply.github.com>
2025-04-08 19:09:34 +0900

    Lab_5 comp.

--- Lab_5/c.py
+++ Lab_5/c.py
@@ -4,6 +4,9 @@ import glm
 import ctypes
 import numpy as np
 
+g_cam_ang = 0.
+g_cam_height = .1
+
 g_vertex_shader_src = '''
 #version 330 core
 
@@ -12,14 +15,14 @@ layout (location = 1) in vec3 vin_color;
 
 out vec4 vout_color;
 
-uniform mat4 M;
+uniform mat4 MVP;
 
 void main()
 {
     // 3D points in homogeneous coordinates
     vec4 p3D_in_hcoord = vec4(vin_pos.xyz, 1.0);
 
-    gl_Position = M * p3D_in_hcoord;
+    gl_Position = MVP * p3D_in_hcoord;
 
     vout_color = vec4(vin_color, 1.);
 }
@@ -83,8 +86,19 @@ def load_shaders(vertex_shader_source, fragment_shader_source):
 
 
 def key_callback(window, key, scancode, action, mods):
+    global g_cam_ang, g_cam_height
     if key==GLFW_KEY_ESCAPE and action==GLFW_PRESS:
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    else:
+        if action==GLFW_PRESS or action==GLFW_REPEAT:
+            if key==GLFW_KEY_1:
+                g_cam_ang += np.radians(-10)
+            elif key==GLFW_KEY_3:
+                g_cam_ang += np.radians(10)
+            elif key==GLFW_KEY_2:
+                g_cam_height += .1
+            elif key==GLFW_KEY_W:
+                g_cam_height += -.1
 
 def prepare_vao_triangle():
     # prepare vertex data (in main memory)
@@ -160,7 +174,7 @@ def main():
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # for macOS
 
     # create a window and OpenGL context
-    window = glfwCreateWindow(800, 800, '2-affine-transform-3D-pyglm', None, None)
+    window = glfwCreateWindow(800, 800, '3-lookat', None, None)
     if not window:
         glfwTerminate()
         return
@@ -173,7 +187,7 @@ def main():
     shader_program = load_shaders(g_vertex_shader_src, g_fragment_shader_src)
 
     # get uniform locations
-    M_loc = glGetUniformLocation(shader_program, 'M')
+    MVP_loc = glGetUniformLocation(shader_program, 'MVP')
     
     # prepare vaos
     vao_triangle = prepare_vao_triangle()
@@ -182,13 +196,25 @@ def main():
     # loop until the user closes the window
     while not glfwWindowShouldClose(window):
         # render
-        glClear(GL_COLOR_BUFFER_BIT)
+
+        # enable depth test (we'll see details later)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
 
         glUseProgram(shader_program)
 
-        # current frame: I (world frame)
+        # projection matrix
+        # use orthogonal projection (we'll see details later)
+        P = glm.ortho(-1,1,-1,1,-1,1)
+
+        # view matrix
+        # rotate camera position with g_cam_ang / move camera up & down with g_cam_height
+        V = glm.lookAt(glm.vec3(.1*np.sin(g_cam_ang),g_cam_height,.1*np.cos(g_cam_ang)), glm.vec3(0,0,0), glm.vec3(0,1,0))
+
+        # current frame: P*V*I (now this is the world frame)
         I = glm.mat4()
-        glUniformMatrix4fv(M_loc, 1, GL_FALSE, glm.value_ptr(I))
+        MVP = P*V*I
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
 
         # draw current frame
         glBindVertexArray(vao_frame)
@@ -214,8 +240,9 @@ def main():
         # M = R @ T
         # M = T @ R
 
-        # current frame: M
-        glUniformMatrix4fv(M_loc, 1, GL_FALSE, glm.value_ptr(M))
+        # current frame: P*V*M
+        MVP = P*V*M
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
 
         # draw triangle w.r.t. the current frame
         glBindVertexArray(vao_triangle)
